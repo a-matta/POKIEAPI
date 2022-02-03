663742ee523d40da9e8e600a7257434a76ceb32b
Amrita Matta <matta.amrita@gmail.com>
2022-02-03 19:54:48 +0200

    added cards in rows

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   return (
@@ -15,21 +17,28 @@ function App() {
           <Navbar.Brand href="#">Navbar</Navbar.Brand>
         </Container>
       </Navbar>
-
-      <Card style={{ width: "18rem" }}>
-        <Card.Img
-          variant="top"
-          img
-          src="https://source.unsplash.com/WrUGh2DXfiw/1600x900"
-        />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <Container>
+        <Row xs={2} md={4} className=" justify-content-between my-4 d-flex g-5">
+          {Array.from({ length: 4 }).map((_, idx) => (
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img
+                  variant="top"
+                  img
+                  src="https://source.unsplash.com/WrUGh2DXfiw/1600x900"
+                />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
