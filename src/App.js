aefd0ff1e23a2872ed5db2408224f01a85b3d1b9
Amrita Matta <matta.amrita@gmail.com>
2022-02-03 19:12:08 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 
 function App() {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <Navbar
+        className="navbar navbar-dark "
+        style={{ backgroundColor: "#FF5733" }}
+      >
+        <Container>
+          <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Card style={{ width: "18rem" }}>
+        <Card.Img
+          variant="top"
+          img
+          src="https://source.unsplash.com/WrUGh2DXfiw/1600x900"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
