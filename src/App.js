b4148d449d16ccd40b262eef43b8ccf296eb8b54
Amrita Matta <matta.amrita@gmail.com>
2022-03-14 20:46:33 +0200

    added skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PokeList from "./components/PokeList";
+import FavList from "./components/FavList";
 
-function App() {
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+
+import "./App.css";
+import PokeSingle from "./components/PokeSingle";
+
+const App = () => {
+  const [favourites, setFavourites] = useState([]);
+  const getArray = JSON.parse(localStorage.getItem("favourites") || "0");
+
+  useEffect(() => {
+    if (getArray !== 0) {
+      setFavourites(getArray);
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    localStorage.setItem("favourites", JSON.stringify(favourites));
+  }, [favourites]);
+
+  const favHandler = (pokemon) => {
+    let item = favourites.some((item) => item.id === pokemon.id);
+
+    if (!item) {
+      setFavourites((prevState) => [...prevState, pokemon]);
+    } else {
+      const newArray = [...favourites];
+      newArray.splice(
+        newArray.findIndex((item) => item.id === pokemon.id),
+        1
+      );
+      setFavourites(newArray);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="pokelist"
+            element={
+              <PokeList favHandler={favHandler} favourites={favourites} />
+            }
+          />
+          <Route
+            path="favourites"
+            element={
+              <FavList
+                favHandler={favHandler}
+                favourites={favourites}
+                removeFav={setFavourites}
+              />
+            }
+          />
+          <Route path="/:pokemonName" element={<PokeSingle />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
